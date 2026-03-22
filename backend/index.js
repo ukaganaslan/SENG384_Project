3887d02523858a157b9629246200a70c92155a69
Ümit Kağan Aslan <c2228006@student.cankaya.edu.tr>
2026-03-22 21:53:55 +0300

    Fixed docker-compose.yml indentation issue

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
